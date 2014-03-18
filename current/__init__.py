b748e85f3e1953f3decee204becbabe12c607fea
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-03-18 10:04:09 -0500

    Updates CSS stylesheet. Adds template for 'current' end-point. Uses the 'current' template using the jinja2 engine.

--- current/__init__.py
+++ current/__init__.py
@@ -4,7 +4,9 @@
 # This script handles the /bitcoin/recent/ end-point, fetching data from the sqlite3 database.
 
 from datetime import datetime
+import jinja2
 import json
+import os
 import sqlite3
 import time
 import urllib2
@@ -26,16 +28,16 @@ def handle(start_response, route):
     now = int(time.time())        # Get current unix time
     timestring = datetime.fromtimestamp(now).strftime('%H:%M')
 
+    # Load the jinja2 template in preparation for rendering:
+    templateLoader = jinja2.FileSystemLoader( searchpath="/" )      # We specify that we will be using absolute paths to specify the location of the template file
+    templateEnv = jinja2.Environment( loader=templateLoader )
 
-    response = """<!DOCTYPE HTML>
-                  <head>
-                    <link rel="stylesheet" type="text/css" href="../static/css/styles.css"/>
-                  </head>
-                  <body>
-                    </br>
-                    <p><b>Current Price:</b> {} - ${:.2f} - ${:.2f}</p>
-                    </br>
-                  </body>""".format(timestring, buy, sell)
+    TEMPLATE_FILE = os.path.join( os.path.dirname(__file__), 'current.html' )
+
+    template = templateEnv.get_template( TEMPLATE_FILE )         # Load template from filesystem
+
+    vars = {"time": timestring, "buy": buy, "sell": sell}       # Create dictionary of variables to be substituted
+    response = template.render( vars ).encode("utf-8")          # jinja2 template renderer returns unicode so we explicitly encode it as utf-8 before returning it so that the browser can read it.
 
     start_response('200 OK', [('Content-Type', 'text/html')])
 
