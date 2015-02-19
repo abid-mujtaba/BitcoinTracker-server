e0aa826c6c7059beed68e972bcb6950241974b51
Abid H Mujtaba <abid.naqvi83@gmail.com>
2015-02-19 15:31:19 +0500

    Add skeleton current end-point. Modify app.py to return current end-point. Add error.py to deal with error in routing in app.py.

--- uwsgi/error.py
+++ uwsgi/error.py
@@ -1,1 +1,8 @@
-__author__ = 'abid'
+# This script is used to generate an error page to indicate to the user that an error has occurred.
+
+
+def handle(start_response, status_code, error_message):
+
+    start_response(status_code, [('Content-Type', 'text/html')])
+
+    return ["<h1>ERROR: {}</h1><p><i>Reason:</i> {}</p>".format(status_code, error_message)]
